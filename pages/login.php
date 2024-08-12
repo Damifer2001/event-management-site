6dd45566f3b80d94b2534db38614f8d3c56f901d
Damifer2001 <141548724+Damifer2001@users.noreply.github.com>
2024-08-12 11:17:02 +0530

    added the logo to title

--- pages/login.php
+++ pages/login.php
@@ -6,6 +6,7 @@
     <title>Register & Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="../css/styles_login.css">
+    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
 </head>
 <body>
     
